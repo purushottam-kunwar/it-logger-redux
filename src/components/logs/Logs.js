a872c07744a07c42c7ee945abff56f2b92b6ffa5
pk.azminds <pk.azminds@gmail.com>
2020-06-29 12:13:32 +0545

    search bar and log component added

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+    /// eslint-disable-next-line
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch("/logs");
+    const data = await res.json();
+    console.log(data);
+
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <h4> Loading... </h4>;
+  }
+
+  return (
+    <div>
+      <ul className="collection-with-header">
+        <li className="collection-header">
+          <h4 className="center"> System logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className="center"> No Logs To show... </p>
+        ) : (
+          logs.map((log) => <li>{log.message}</li>)
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Logs;
